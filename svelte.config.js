b4cbfa154b7082e6c804d0d3b2eb248109e26c49
Brandon Dcruz <bdcruz128@gmail.com>
2022-05-16 16:30:24 -0500

    gh pages deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,22 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from "@sveltejs/adapter-static"; 
+// was "@sveltejs/adapter-auto"
 import preprocess from 'svelte-preprocess';
 
-/** @type {import('@sveltejs/kit').Config} */
+/** @type {import(""@sveltejs/kit").Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
-
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		adapter: adapter({
-			pages: 'docs',
-			assets: 'docs',
-			fallback: null
-		})
-	}
+    kit: {
+        adapter: adapter({
+            pages: "docs",
+            assets: "docs"
+        }),
+        paths: {
+            // change below to your repo name
+            base: "",
+        }
+    }
 };
 
 export default config;
